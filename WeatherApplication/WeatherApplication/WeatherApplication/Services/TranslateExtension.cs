4176c5b45a53de2295a909e8b9e95f05938d2808
Julian Pasqué <julian.pasque@cgi.com>
2019-01-11 12:08:31 +0100

    Added resources

---
+++ WeatherApplication/WeatherApplication/WeatherApplication/Services/TranslateExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApplication.Services
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "LocalizationDemo.Resources.AppResource";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+            ResourceManager resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
